c86bbcdfcbea16d15ee18011360a287df743805e
Gustx21 <145871288+Gustx21@users.noreply.github.com>
2024-03-07 17:09:37 -0300

    Legibilidade do Código

--- js/script.js
+++ js/script.js
@@ -14,29 +14,16 @@ function imc() {
 
     if (valorIMC < 18.5) {
         grau = 'abaixo do peso.';
-    }
-    else {
-        if (valorIMC < 25) {
-            grau = 'com peso ideal. Parabéns!!!';
-        }
-        else {
-            if (valorIMC < 30) {
-                grau = 'levemente acima do peso.';
-            }
-            else {
-                if (valorIMC < 35) {
-                    grau = 'com obesidade grau I.';
-                }
-                else {
-                    if (valorIMC < 40) {
-                        grau = 'com obesidade grau II';
-                    }
-                    else {
-                        grau = 'com obesidade grau III. Cuidado!!';
-                    }
-                }
-            }
-        }
+    } else if (valorIMC < 25) {
+        grau = 'com peso ideal. Parabéns!';
+    } else if (valorIMC < 30) {
+        grau = 'levemente acima do peso.';
+    } else if (valorIMC < 35) {
+        grau = 'com obesidade grau I.';
+    } else if (valorIMC < 40) {
+        grau = 'com obesidade grau II.';
+    } else {
+        grau = 'com obesidade grau III. Cuidado!!!';
     }
 
     resultado.textContent = `Seu IMC é ${valorIMC} e você está ${grau}`;
